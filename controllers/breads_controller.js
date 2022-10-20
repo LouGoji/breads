ec5395076e4c7717fe70bfee793ebc4eb2bb8112
Louis Mantell <loiuismantell@Louis-iMac.local>
2022-10-19 20:01:52 -0400

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,8 +4,14 @@ const Bread = require('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-  res.send(Bread)
+  res.render('Index',
+    {
+      breads: Bread,
+      title: 'Index Page'
+    }
+  )
 })
+
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
