b06793ef1264ad22e16dfa37f1fb94515c754efb
Louis Mantell <loiuismantell@Louis-iMac.local>
2022-10-22 09:58:33 -0400

    sending index and show data

--- server.js
+++ server.js
@@ -16,10 +16,15 @@ app.get('/', (req, res) => {
   app.use('/breads', breadsController)
 
 // MIDDLEWARE
+app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
 
 // LISTEN
 app.listen(PORT, () => {
