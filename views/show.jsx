aabd2696199ce82965937835999c4a3dccd1473c
Louis Mantell <loiuismantell@Louis-iMac.local>
2022-10-24 18:28:34 -0400

    delete route complete

--- views/show.jsx
+++ views/show.jsx
@@ -18,8 +18,8 @@ function Show ({bread, index}) {
   </p>
   <img src={bread.image} alt={bread.name} />
   <li><a href="/breads">Go home</a></li>
-  
-  <form action={`/breads/${index}?_method=DELETE`} method="POST">
+
+<form action={`/breads/${index}?_method=DELETE`} method="POST">
   <input type='submit' value="DELETE"/>
 </form>
 
