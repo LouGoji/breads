547ffcbde0ea8ac4e38233c8be4e2f09ad2aa905
Louis Mantell <loiuismantell@Louis-iMac.local>
2022-10-29 10:07:31 -0400

    mongoose complete

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // DEPENDENCIES
 const express = require('express')
 const methodOverride = require('method-override')
+const mongoose = require('mongoose')
+
 
 // CONFIGURATION
 require('dotenv').config()
@@ -33,3 +35,6 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
 })
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
